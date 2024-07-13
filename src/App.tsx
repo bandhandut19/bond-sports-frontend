a759637ff522889a09695b573fd8c3b7a3b7569d
bandhandut19 <bondon21081@gmail.com>
2024-07-13 10:58:07 +0600

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <Button>Hellooooo Billuuuuuuuu</Button>
+    </div>
+  );
+};
+
+export default App;
