65224b70a8659fd8a4bd450dbdaa8ec453b1b1a0
bandhandut19 <bondon21081@gmail.com>
2024-08-06 21:40:11 +0600

    filter ui design completed

--- src/pages/AllProducts.tsx
+++ src/pages/AllProducts.tsx
@@ -1,6 +1,4 @@
-// import { Button } from "@/components/ui/button";
 import { useGetAllProductsQuery } from "@/redux/features/product/productApi";
-// import { Link } from "react-router-dom";
 import { TProduct } from "../types/ProductType";
 import ProductCard from "@/components/ui/ProductCard";
 import { Button } from "@/components/ui/button";
@@ -8,7 +6,15 @@ import { FaSearch } from "react-icons/fa";
 import { Input } from "@/components/ui/input";
 import { FaFilter } from "react-icons/fa";
 
-import { Label } from "@/components/ui/label";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import {
   Sheet,
   SheetClose,
@@ -21,8 +27,13 @@ import {
 } from "@/components/ui/sheet";
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { AllCategories } from "@/types/ProductCategories";
 
 const AllProducts = () => {
+  const [categoryPosition, setCategoryPosition] = useState("Select category");
+  const [sortPosition, setSortPosition] = useState("Sort By");
+  const [brandPosition, setBrandPosition] = useState("Select Brand");
+  const [orderPosition, setOrderPosition] = useState("Select Order");
   const [searchParams] = useSearchParams();
   const category = searchParams.get("category") || "";
   const [searchName, setSearchName] = useState("");
@@ -33,8 +44,18 @@ const AllProducts = () => {
   });
   const productData = data?.data || [];
 
-  const handleApplyFilter = (e: React.FormEvent<HTMLFormElement>) => {
+  // const handleApplyFilter = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  // };
+  const handleClearFilters = (e) => {
     e.preventDefault();
+    setCategoryPosition("Select category");
+    setSortPosition("Sort By");
+    setBrandPosition("Select Brand");
+    setOrderPosition("Select Order");
+    const form = e.target;
+    form.minimumPrice.value = 0;
+    form.maximumPrice.value = 100;
   };
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -93,41 +114,152 @@ const AllProducts = () => {
                   Apply filters to get the products as per your need
                 </SheetDescription>
               </SheetHeader>
-              <form onSubmit={handleApplyFilter}>
+              <form onSubmit={handleClearFilters}>
                 <div className="grid gap-4 py-4">
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="name" className="text-right">
-                      Name
-                    </Label>
-                    <Input
-                      id="name"
-                      value="Pedro Duarte"
-                      className="col-span-3"
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="outline">{categoryPosition}</Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                      <DropdownMenuSeparator />
+                      <DropdownMenuRadioGroup
+                        value={categoryPosition}
+                        onValueChange={setCategoryPosition}
+                      >
+                        <DropdownMenuRadioItem value="All Categories">
+                          All Categories
+                        </DropdownMenuRadioItem>
+                        {AllCategories.map((category) => (
+                          <DropdownMenuRadioItem
+                            key={category}
+                            value={category}
+                          >
+                            {category}
+                          </DropdownMenuRadioItem>
+                        ))}
+                      </DropdownMenuRadioGroup>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                  <div className="grid  gap-4">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button variant="outline">{sortPosition}</Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent className="w-56">
+                        <DropdownMenuSeparator />
+                        <DropdownMenuRadioGroup
+                          value={categoryPosition}
+                          onValueChange={setSortPosition}
+                        >
+                          <DropdownMenuRadioItem value="Price">
+                            Price
+                          </DropdownMenuRadioItem>
+                          <DropdownMenuRadioItem value="Rating">
+                            Rating
+                          </DropdownMenuRadioItem>
+                        </DropdownMenuRadioGroup>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
+                  <div className="grid  gap-4">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button variant="outline">{orderPosition}</Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent className="w-56">
+                        <DropdownMenuSeparator />
+                        <DropdownMenuRadioGroup
+                          value={categoryPosition}
+                          onValueChange={setOrderPosition}
+                        >
+                          <DropdownMenuRadioItem value="Ascending">
+                            Ascending
+                          </DropdownMenuRadioItem>
+                          <DropdownMenuRadioItem value="Descending">
+                            Descending
+                          </DropdownMenuRadioItem>
+                        </DropdownMenuRadioGroup>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
+                  <div className="grid  gap-4">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button variant="outline">{brandPosition}</Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent className="w-56">
+                        <DropdownMenuSeparator />
+                        <DropdownMenuRadioGroup
+                          value={categoryPosition}
+                          onValueChange={setBrandPosition}
+                        >
+                          <DropdownMenuRadioItem value="All Brands">
+                            All Brands
+                          </DropdownMenuRadioItem>
+                          <DropdownMenuRadioItem value="SG">
+                            SG
+                          </DropdownMenuRadioItem>
+                          <DropdownMenuRadioItem value="Adidas">
+                            Adidas
+                          </DropdownMenuRadioItem>
+                          <DropdownMenuRadioItem value="Nike">
+                            Nike
+                          </DropdownMenuRadioItem>
+                          <DropdownMenuRadioItem value="Kookaburra">
+                            Kookaburra
+                          </DropdownMenuRadioItem>
+                          <DropdownMenuRadioItem value="Reebok">
+                            Reebok
+                          </DropdownMenuRadioItem>
+                          <DropdownMenuRadioItem value="Puma">
+                            Puma
+                          </DropdownMenuRadioItem>
+                          <DropdownMenuRadioItem value="Under Armour">
+                            Under Armour
+                          </DropdownMenuRadioItem>
+                          <DropdownMenuRadioItem value="Asics">
+                            Asics
+                          </DropdownMenuRadioItem>
+                        </DropdownMenuRadioGroup>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
+                  <div className="grid gap-4">
+                    <label htmlFor="minPrice" className="font-semibold">
+                      Set Minimum Price ( BDT )
+                    </label>
+                    <input
+                      className="text-center py-2 rounded-md"
+                      type="number"
+                      name="minimumPrice"
+                      defaultValue={0}
                     />
                   </div>
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="username" className="text-right">
-                      Username
-                    </Label>
-                    <Input
-                      id="username"
-                      value="@peduarte"
-                      className="col-span-3"
+                  <div className="grid gap-4">
+                    <label htmlFor="minPrice" className="font-semibold">
+                      Set Maximum Price ( BDT )
+                    </label>
+                    <input
+                      className="text-center py-2 rounded-md"
+                      type="number"
+                      name="maximumPrice"
+                      defaultValue={100}
                     />
                   </div>
                 </div>
                 <SheetFooter>
-                  <SheetClose asChild>
-                    <Button
-                      className="border-2 border-slate-400 "
-                      type="submit"
-                    >
-                      Apply Filters
-                    </Button>
-                  </SheetClose>
-                  <Button className="border-2 border-slate-400" type="submit">
+                  {/* <SheetClose asChild> */}
+                  <Button
+                    className="border-2 border-slate-400 "
+                    type="submit"
+                    // onClick={handleClearFilters}
+                  >
                     Clear Filters
                   </Button>
+                  {/* </SheetClose> */}
+                  {/* <Button className="border-2 border-slate-400" type="submit">
+                  Apply Filters
+                  </Button> */}
                 </SheetFooter>
               </form>
             </SheetContent>
