7dccbfaa9e135571f9a93a99ff7dad7b4626a346
bandhandut19 <bondon21081@gmail.com>
2024-08-18 02:16:44 +0600

    added contact form section in home page

---
+++ src/components/ui/ContactForm.tsx
@@ -0,0 +1,113 @@
+import TopMarginSetter from "@/utils/TopMarginSetter";
+import { MdEmail } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { Button } from "./button";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "./input";
+import { useForm } from "react-hook-form";
+import { TContactForm } from "@/types/contact";
+
+const ContactForm = () => {
+  const { register, handleSubmit } = useForm<TContactForm>();
+  const handleContactForm = (data: TContactForm) => {
+    console.log(data);
+    //! here form filled data will be retrived.. in future will make the button functional to send as email.
+  };
+  return (
+    <div>
+      <TopMarginSetter></TopMarginSetter>
+      <div className="grid grid-cols-2 gap-5 bg-yellow-400 py-6 px-3 rounded-md">
+        <div className="bg-orange-400 bg-opacity-75 px-8 py-5 rounded-md">
+          <h1 className="text-3xl font-extrabold mb-10 text-white">
+            Get In Touch
+          </h1>
+
+          <form
+            className="flex flex-col gap-2"
+            onSubmit={handleSubmit(handleContactForm)}
+          >
+            <Label htmlFor="name" className="font-bold">
+              Name
+            </Label>
+            <Input
+              className="mb-5 mt-2 bg-yellow-300 text-black placeholder:text-black placeholder:text-opacity-35 placeholder:font-bold"
+              type="text"
+              id="name"
+              required
+              {...register("name")}
+              placeholder="Enter your name"
+            ></Input>
+            <Label htmlFor="email" className="font-bold">
+              Email
+            </Label>
+            <Input
+              className="mb-5 mt-2 bg-yellow-300 placeholder:text-black placeholder:text-opacity-35 placeholder:font-bold"
+              placeholder="Enter your email"
+              {...register("email")}
+              required
+              id="email"
+            ></Input>
+            <Label htmlFor="message" className="font-bold">
+              Message
+            </Label>
+            <textarea
+              className="mb-5 bg-yellow-300 text-left px-2 py-2 rounded-md placeholder:text-black placeholder:text-opacity-35 placeholder:font-bold"
+              placeholder="Enter your message here"
+              {...register("message")}
+              required
+            ></textarea>
+            <Button type="submit" className="hover:bg-white hover:text-lg">
+              Send Message
+            </Button>
+          </form>
+        </div>
+        <div className="bg-orange-400 bg-opacity-75  px-8 py-5 rounded-md">
+          <h1 className="text-3xl text-right font-extrabold mb-10 text-white">
+            Contact Information
+          </h1>
+          <div className="items-center justify-center flex gap-3 flex-col text-center">
+            <div>
+              <div className="text-center justify-center items-center flex flex-col">
+                <FaLocationDot className="text-4xl text-red-600" />
+              </div>
+              <h1 className="text-center text-slate-700 text-xl font-extrabold outline-dashed px-5 mt-3 mb-3">
+                Store Location
+              </h1>
+              <div>
+                <h1>
+                  1234 Ojana Road, <br />
+                  Merul Badda , Dhaka
+                </h1>
+              </div>
+            </div>
+            <div>
+              <div className="text-center justify-center items-center flex flex-col">
+                <MdEmail className="text-4xl text-yellow-300" />
+              </div>
+              <h1 className="text-center text-slate-700 text-xl font-extrabold outline-dashed px-5 mt-3 mb-3">
+                Email
+              </h1>
+              <div>
+                <h1>bondon21081@gmail.com</h1>
+              </div>
+            </div>
+            <div>
+              <div className="text-center justify-center items-center flex flex-col">
+                <FaPhoneAlt className="text-4xl text-green-600" />
+              </div>
+              <h1 className="text-center text-slate-700 text-xl font-extrabold outline-dashed px-5 mt-3 mb-3">
+                Contact No
+              </h1>
+              <div>
+                <h1>01797537300</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import Categories from "@/components/ui/Categories";
+import ContactForm from "@/components/ui/ContactForm";
 import FeaturedProducts from "@/components/ui/FeaturedProducts";
 import PageSlider from "@/components/ui/PageSlider";
 import { sliderPhotos } from "@/utils/sliderPhotos";
@@ -20,6 +21,12 @@ const Home = () => {
       </span>
       <TopMarginSetter></TopMarginSetter>
       <Categories></Categories>
+      <TopMarginSetter></TopMarginSetter>
+      <span className="lg:text-3xl text-4xl font-bold border-b-4 border-l-4 px-2 py-1 border-yellow-400 border-opacity-70 rounded-md">
+        Contact Us
+      </span>
+      <ContactForm></ContactForm>
+      <TopMarginSetter></TopMarginSetter>
     </div>
   );
 };
