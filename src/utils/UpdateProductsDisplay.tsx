193e2b4358430964abf800e1dba0420806ca3367
bandhandut19 <bondon21081@gmail.com>
2024-08-19 03:20:21 +0600

    fixed update products header alignment

--- src/utils/UpdateProductsDisplay.tsx
+++ src/utils/UpdateProductsDisplay.tsx
@@ -51,7 +51,7 @@ const UpdateProductsDisplay = () => {
 
   return (
     <div className="min-h-screen">
-      <h1 className="mb-8 cursor-pointer text-2xl w-9/12 mx-auto font-bold border-4 px-4 py-1 rounded-full bg-orange-600 border-l-0 border-t-0 border-slate-500 text-white border-e-6 ">
+      <h1 className="mb-8 text-center cursor-pointer text-2xl w-9/12 mx-auto font-bold border-4 px-4 py-1 rounded-full bg-orange-600 border-l-0 border-t-0 border-slate-500 text-white border-e-6 ">
         Update Products
       </h1>
       <Table>
