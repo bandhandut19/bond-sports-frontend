b150dfa1310201f0c93ebfd345717e4009fd413a
bandhandut19 <bondon21081@gmail.com>
2024-12-10 11:47:49 +0600

    added blogs section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import FeaturedProducts from "@/components/ui/FeaturedProducts";
 import PageSlider from "@/components/ui/PageSlider";
 import { sliderPhotos } from "@/utils/sliderPhotos";
 import TopMarginSetter from "@/utils/TopMarginSetter";
+import Blog from "./Blogs";
 
 const Home = () => {
   return (
@@ -20,8 +21,12 @@ const Home = () => {
       </h1>
       <TopMarginSetter></TopMarginSetter>
       <Categories></Categories>
-
       <TopMarginSetter></TopMarginSetter>
+      <h1 className="lg:text-4xl text-4xl font-bold text-center  border-b-4 border-l-4 px-2 py-6 border-orange-600  bg-yellow-400 border-opacity-70 rounded-md">
+        Blogs
+      </h1>
+      <TopMarginSetter></TopMarginSetter>
+      <Blog></Blog>
     </div>
   );
 };
