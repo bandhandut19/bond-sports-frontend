61fdd5a246a7653cf7ac666818de71568fb78217
bandhandut19 <bondon21081@gmail.com>
2024-12-13 20:58:32 +0600

    added new section named dealsOftheDay

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import Blog from "./Blogs";
 import PageSlider from "@/components/ui/PageSlider";
 import { sliderPhotos } from "@/utils/sliderPhotos";
 import TopSellingProducts from "@/components/ui/TopSellingProducts";
+import DealsOfTheDay from "@/components/ui/DealsOfTheDay";
 const Home = () => {
   return (
     <div>
@@ -28,6 +29,12 @@ const Home = () => {
         </h1>
         <TopMarginSetter></TopMarginSetter>
         <Categories></Categories>
+        <TopMarginSetter></TopMarginSetter>
+        <h1 className="lg:text-4xl text-4xl font-bold text-center  border-b-4 border-l-4 px-2 py-6 border-orange-600  bg-yellow-400 border-opacity-70 rounded-md">
+          Deals of the day
+        </h1>
+        <TopMarginSetter></TopMarginSetter>
+        <DealsOfTheDay></DealsOfTheDay>
         <TopMarginSetter></TopMarginSetter>
         <h1 className="lg:text-4xl text-4xl font-bold text-center  border-b-4 border-l-4 px-2 py-6 border-orange-600  bg-yellow-400 border-opacity-70 rounded-md">
           Blogs
