cd583f5158b471088bd3ffc231e1bddbd7dbb8b7
bandhandut19 <bondon21081@gmail.com>
2024-12-16 14:10:52 +0600

    used redux persist to persist data

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.ts";
+import { persistor, store } from "./redux/store.ts";
 import { RouterProvider } from "react-router-dom";
 import root from "./routes/root.tsx";
 import { Toaster } from "sonner";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Toaster position="top-center" theme="system" richColors></Toaster>
-      <RouterProvider router={root}></RouterProvider>
-      <ToastContainer></ToastContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <Toaster position="top-center" theme="system" richColors></Toaster>
+        <RouterProvider router={root}></RouterProvider>
+        <ToastContainer></ToastContainer>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,17 +3,41 @@ import { baseApi } from "./api/baseApi";
 import productReducer from "./features/product/productSlice";
 import cartReducer from "./features/cart/cartSlice";
 import authReducer from "./features/auth/authSlice";
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedProductReducer = persistReducer(persistConfig, productReducer);
+const persistedCartReducer = persistReducer(persistConfig, cartReducer);
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
-    product: productReducer,
-    cart: cartReducer,
-    auth: authReducer,
+    product: persistedProductReducer,
+    cart: persistedCartReducer,
+    auth: persistedAuthReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(baseApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 export default store;
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
